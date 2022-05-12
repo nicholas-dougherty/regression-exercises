b5787b06b41e3af43dd2f5419e05d2e8610b3347
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-12 13:52:03 -0500

    updated explore.py

--- explore.py
+++ explore.py
@@ -1,27 +1,43 @@
+#     ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄     #   
+#    ▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌    #   
+#    ▐░█▀▀▀▀▀▀▀▀▀  ▐░▌   ▐░▌ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀     #   
+#    ▐░▌            ▐░▌ ▐░▌  ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌              #   
+#    ▐░█▄▄▄▄▄▄▄▄▄    ▐░▐░▌   ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄     #   
+#    ▐░░░░░░░░░░░▌    ▐░▌    ▐░░░░░░░░░░░▌▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌    #   
+#    ▐░█▀▀▀▀▀▀▀▀▀    ▐░▌░▌   ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌          ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀     #   
+#    ▐░▌            ▐░▌ ▐░▌  ▐░▌          ▐░▌          ▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌              #   
+#    ▐░█▄▄▄▄▄▄▄▄▄  ▐░▌   ▐░▌ ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄     #   
+#    ▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌    #   
+#     ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀     #   
+#                                                                                                 #   
+#|-----------------------------------------------------------------------------------------------|#
+#|-----------------------------------------------------------------------------------------------|#
+# IMPORTS
 import pandas as pd 
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
+#|-----------------------------------------------------------------------------------------------|#
+#|-----------------------------------------------------------------------------------------------|#
 def plot_variable_pairs(train, cols, hue=None):
     '''
     This function takes in a df, a list of cols to plot, and default hue=None 
     and displays a pairplot with a red regression line.
     '''
     plot_kws={'line_kws':{'color':'red'}, 'scatter_kws': {'alpha': 0.7}}
-    sns.pairplot(train[cols], hue=hue, kind="reg",plot_kws={'line_kws':{'color':'red'}, 'scatter_kws': {'alpha': 0.1}})
+    sns.pairplot(train[cols], hue=hue, kind="reg",plot_kws={'line_kws':{'color':'red'},
+                                                            'scatter_kws': {'alpha': 0.1}})
     plt.show()
-
+#|-----------------------------------------------------------------------------------------------|#
 def months_to_years(train, validate, test):
     train['tenure_years'] = (train.tenure / 12).astype(int)
     validate['tenure_years'] = (validate.tenure / 12).astype(int)
     test['tenure_years'] = (test.tenure / 12).astype(int)
 
     return train, validate, test 
-
+#|-----------------------------------------------------------------------------------------------|#
 def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
     """
     Accepts a string name of a categorical variable, 
@@ -51,6 +67,5 @@ def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
     sns.catplot(x=categorical_var, y=continuous_var, data=df, kind="bar", palette='dark')
     plt.xlabel(categorical_var, fontsize=12)
     plt.ylabel(continuous_var, fontsize=12)
-
-
-
+#|-----------------------------------------------------------------------------------------------|#
+#|-----------------------------------------------------------------------------------------------|#
